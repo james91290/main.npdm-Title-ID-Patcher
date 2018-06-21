6ca5da20f8ab1ee8a9e6d36e97423e01a72551ba
james91290 <40446771+james91290@users.noreply.github.com>
2018-06-21 10:12:30 +0800

    Add files via upload

---
+++ main.npdm Title ID Patcher/frmMain.Designer.vb
@@ -0,0 +1,227 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.btnPatch = New System.Windows.Forms.Button()
+        Me.txtTitleID = New System.Windows.Forms.TextBox()
+        Me.txtExefsPath = New System.Windows.Forms.TextBox()
+        Me.cmbTitles = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnNew = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnContextMenu = New System.Windows.Forms.Button()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnPatch
+        '
+        Me.btnPatch.Enabled = False
+        Me.btnPatch.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPatch.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btnPatch.Location = New System.Drawing.Point(118, 206)
+        Me.btnPatch.Name = "btnPatch"
+        Me.btnPatch.Size = New System.Drawing.Size(125, 33)
+        Me.btnPatch.TabIndex = 0
+        Me.btnPatch.Text = "PATCH"
+        Me.btnPatch.UseVisualStyleBackColor = True
+        '
+        'txtTitleID
+        '
+        Me.txtTitleID.Location = New System.Drawing.Point(205, 38)
+        Me.txtTitleID.Name = "txtTitleID"
+        Me.txtTitleID.Size = New System.Drawing.Size(135, 26)
+        Me.txtTitleID.TabIndex = 1
+        Me.txtTitleID.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtExefsPath
+        '
+        Me.txtExefsPath.AllowDrop = True
+        Me.txtExefsPath.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtExefsPath.Location = New System.Drawing.Point(9, 40)
+        Me.txtExefsPath.Multiline = True
+        Me.txtExefsPath.Name = "txtExefsPath"
+        Me.txtExefsPath.Size = New System.Drawing.Size(263, 35)
+        Me.txtExefsPath.TabIndex = 2
+        '
+        'cmbTitles
+        '
+        Me.cmbTitles.FormattingEnabled = True
+        Me.cmbTitles.Location = New System.Drawing.Point(6, 38)
+        Me.cmbTitles.Name = "cmbTitles"
+        Me.cmbTitles.Size = New System.Drawing.Size(193, 26)
+        Me.cmbTitles.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(6, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(184, 15)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Folder that contains ""romfs.bin"""
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Black
+        Me.Label2.Location = New System.Drawing.Point(6, 22)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(65, 15)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Game Title"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Black
+        Me.Label3.Location = New System.Drawing.Point(233, 22)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 15)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Game ID"
+        '
+        'btnNew
+        '
+        Me.btnNew.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNew.ForeColor = System.Drawing.Color.Black
+        Me.btnNew.Location = New System.Drawing.Point(110, 70)
+        Me.btnNew.Name = "btnNew"
+        Me.btnNew.Size = New System.Drawing.Size(58, 23)
+        Me.btnNew.TabIndex = 5
+        Me.btnNew.Text = "NEW"
+        Me.btnNew.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDelete.ForeColor = System.Drawing.Color.Black
+        Me.btnDelete.Location = New System.Drawing.Point(174, 70)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(62, 23)
+        Me.btnDelete.TabIndex = 6
+        Me.btnDelete.Text = "DELETE"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtTitleID)
+        Me.GroupBox1.Controls.Add(Me.btnDelete)
+        Me.GroupBox1.Controls.Add(Me.cmbTitles)
+        Me.GroupBox1.Controls.Add(Me.btnNew)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Font = New System.Drawing.Font("Calibri", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.ForeColor = System.Drawing.Color.Blue
+        Me.GroupBox1.Location = New System.Drawing.Point(8, 97)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(348, 103)
+        Me.GroupBox1.TabIndex = 7
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GAME INSTALLED ON NINTENDO SWITCH"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btnContextMenu)
+        Me.GroupBox2.Controls.Add(Me.btnBrowse)
+        Me.GroupBox2.Controls.Add(Me.txtExefsPath)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Font = New System.Drawing.Font("Calibri", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.ForeColor = System.Drawing.Color.Blue
+        Me.GroupBox2.Location = New System.Drawing.Point(8, 7)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(348, 84)
+        Me.GroupBox2.TabIndex = 7
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "GAME TO BE PATCH"
+        '
+        'btnContextMenu
+        '
+        Me.btnContextMenu.Font = New System.Drawing.Font("Calibri", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnContextMenu.Location = New System.Drawing.Point(205, 16)
+        Me.btnContextMenu.Name = "btnContextMenu"
+        Me.btnContextMenu.Size = New System.Drawing.Size(135, 20)
+        Me.btnContextMenu.TabIndex = 8
+        Me.btnContextMenu.Text = "Add to Context Menu"
+        Me.btnContextMenu.UseVisualStyleBackColor = True
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBrowse.ForeColor = System.Drawing.Color.Black
+        Me.btnBrowse.Location = New System.Drawing.Point(278, 40)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(62, 35)
+        Me.btnBrowse.TabIndex = 5
+        Me.btnBrowse.Text = "BROWSE"
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(367, 245)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnPatch)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "main.npdm Title ID Patcher"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnPatch As Button
+    Friend WithEvents txtTitleID As TextBox
+    Friend WithEvents txtExefsPath As TextBox
+    Friend WithEvents cmbTitles As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnNew As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents btnBrowse As Button
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents btnContextMenu As Button
+End Class
---
+++ main.npdm Title ID Patcher/frmMain.vb
@@ -0,0 +1,251 @@
+﻿Imports System.IO
+Imports System.Collections
+Imports System.Text.RegularExpressions
+Imports System.Security.Principal
+
+Public Class frmMain
+    Dim DBGameIDPath As String = Application.StartupPath & "\gametitles.txt"
+
+    Dim identity = WindowsIdentity.GetCurrent()
+    Dim principal = New WindowsPrincipal(identity)
+    Dim isElevated As Boolean = principal.IsInRole(WindowsBuiltInRole.Administrator)
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnPatch.Click
+        Dim folderPath As String = Microsoft.VisualBasic.Left(txtExefsPath.Text, Len(txtExefsPath.Text) - 6)
+        Dim pFolder As String = Microsoft.VisualBasic.Left(txtExefsPath.Text, Len(txtExefsPath.Text) - 17)
+        Try
+            If Directory.Exists(pFolder & "\" & txtTitleID.Text) Then
+                MsgBox("Game ID: " & txtTitleID.Text & " is already exist!")
+                Exit Sub
+            End If
+
+            ' CloseFolder(pFolder & "\")
+            Dim targetTitleIdULong As ULong = Convert.ToUInt64(txtTitleID.Text, 16)
+            Dim npdmFilePath As String = Path.Combine(txtExefsPath.Text & "\exefs", "main.npdm")
+            Dim npdmBytes As Byte() = File.ReadAllBytes(npdmFilePath)
+            Dim patchedNpdmBytes As Byte() = getPatchedNpdmBytes(npdmBytes, targetTitleIdULong)
+            File.Delete(npdmFilePath)
+            File.WriteAllBytes(npdmFilePath, patchedNpdmBytes)
+            FileIO.FileSystem.RenameDirectory(txtExefsPath.Text, txtTitleID.Text)
+            txtExefsPath.Text = Microsoft.VisualBasic.Left(txtExefsPath.Text, Len(txtExefsPath.Text) - 16) & txtTitleID.Text
+            MsgBox("Successfully Patched!", vbInformation)
+            If isArg = True Then End
+            ' Process.Start(pFolder)
+            'End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+
+    Public Sub CloseFolder(myfolder As String)
+        Dim OpenFolder As Object = CreateObject("shell.application")
+        For Each item In OpenFolder.Windows
+            If item.document.folder.self.Path = myfolder Then
+                item.Quit()
+            End If
+        Next
+    End Sub
+    Public Function getPatchedNpdmBytes(fileBytes As Byte(), targetTitleId As ULong) As Byte()
+        Dim aci0RawOffset As Integer = BitConverter.ToInt32(fileBytes, 112)
+        'If fileBytes(aci0RawOffset) <> 65 Or fileBytes(aci0RawOffset) <> 67 Or fileBytes(aci0RawOffset) <> 73 Or fileBytes(aci0RawOffset) <> 48 Then
+        '    MsgBox("Unable to decrypt NCA file, check your keyset! You should remove created files.")
+        '    Exit Function
+        'End If
+        Dim TitleIdBytes As Byte() = BitConverter.GetBytes(targetTitleId)
+        Array.Copy(TitleIdBytes, 0, fileBytes, aci0RawOffset + 16, TitleIdBytes.Length)
+        Return fileBytes
+    End Function
+
+    Private Sub txtExefsPath_DragDrop(sender As Object, e As DragEventArgs) Handles txtExefsPath.DragDrop, Me.DragDrop
+        Dim DroppedFiles() As String = e.Data.GetData(DataFormats.FileDrop)
+        Try
+            For Each Entry As String In DroppedFiles
+                txtExefsPath.Text = Entry
+            Next
+            cmbTitles.Focus()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+    Private Sub txtExefsPath_DragEnter(sender As Object, e As DragEventArgs) Handles txtExefsPath.DragEnter, Me.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then e.Effect = DragDropEffects.Copy
+    End Sub
+    Sub LoadTitles()
+        Try
+            btnDelete.Enabled = False
+            Dim xRead As IO.StreamReader
+            Dim words As New Dictionary(Of String, String)()
+            xRead = File.OpenText(DBGameIDPath)
+            Do Until xRead.EndOfStream
+                Dim strArr() As String = xRead.ReadLine.Split(""",""")
+                words.Add(strArr(1), strArr(3))
+            Loop
+            cmbTitles.DataSource = New BindingSource(words, Nothing)
+            cmbTitles.DisplayMember = "Value"
+            cmbTitles.ValueMember = "Key"
+            xRead.Close()
+            If cmbTitles.Items.Count > 0 Then btnDelete.Enabled = True Else btnDelete.Enabled = False
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+    Private Function ValidatePath(Path As String) As Boolean
+        ValidatePath = False
+        If File.Exists(Path & "\romfs.bin") And File.Exists(Path & "\exefs\main.npdm") Then txtExefsPath.ForeColor = Color.Black : Return True : Exit Function
+        txtExefsPath.ForeColor = Color.Red
+    End Function
+    Dim isArg As Boolean = False
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If My.Computer.Registry.GetValue("HKEY_CLASSES_ROOT\Folder\shell\Patch Game ID", "Icon", Nothing) Is Nothing Then btnContextMenu.Text = "Add to Context Menu" Else btnContextMenu.Text = "Remove from Context Menu"
+
+        For Each arg As String In My.Application.CommandLineArgs
+            If ValidatePath(arg.ToString) = True Then
+                txtExefsPath.Text = arg.ToString
+                isArg = True
+            ElseIf arg.ToString <> "" Then
+                MsgBox("Selected folder doesn't contains romfs.bin and main.npdm", vbInformation)
+                End
+            End If
+        Next
+
+        LoadTitles()
+
+        'cmbTitles.DataSource = New BindingSource(TARGET_TITLES, Nothing)
+        'cmbTitles.DisplayMember = "Value"
+        'cmbTitles.ValueMember = "Key"
+    End Sub
+
+
+    Private Sub cmbTitles_TextChanged(sender As Object, e As EventArgs) Handles cmbTitles.TextChanged
+        Try
+            Dim key As String = DirectCast(cmbTitles.SelectedItem, KeyValuePair(Of String, String)).Key
+            'Dim value As String = DirectCast(cmbTitles.SelectedItem, KeyValuePair(Of String, String)).Value
+            txtTitleID.Text = StrConv(key, vbUpperCase)
+        Catch ex As Exception
+            If btnPatch.Enabled = True Then txtTitleID.Text = ""
+        End Try
+        If btnDelete.Text = "SAVE" Then
+            If cmbTitles.Text <> "" And (New Regex("^[a-fA-F0-9]{16}$")).Match(txtTitleID.Text).Success Then btnDelete.Enabled = True Else btnDelete.Enabled = False
+        End If
+    End Sub
+
+    Private Sub txtTitleID_TextChanged(sender As Object, e As EventArgs) Handles txtTitleID.TextChanged, txtExefsPath.TextChanged
+        On Error Resume Next
+        Dim pFolder As String = Microsoft.VisualBasic.Left(txtExefsPath.Text, Len(txtExefsPath.Text) - 16)
+        Dim isFolderExist As Boolean = False
+        For Each Dir As String In Directory.GetDirectories(pFolder)
+            If Dir.Remove(0, pFolder.Length) = txtTitleID.Text Then isFolderExist = True
+        Next
+        If Microsoft.VisualBasic.Right(txtExefsPath.Text, 16) = txtTitleID.Text Or isFolderExist = True Then txtTitleID.ForeColor = Color.Red Else txtTitleID.ForeColor = Color.Black
+        If txtTitleID.ForeColor = Color.Black And ValidatePath(txtExefsPath.Text) = True And txtTitleID.Text <> "" And
+            (New Regex("^[a-fA-F0-9]{16}$")).Match(txtTitleID.Text).Success And btnDelete.Text = "DELETE" Then
+            btnPatch.Enabled = True
+        Else
+            btnPatch.Enabled = False
+        End If
+        If btnDelete.Text = "SAVE" Then
+            If cmbTitles.Text <> "" And (New Regex("^[a-fA-F0-9]{16}$")).Match(txtTitleID.Text).Success Then btnDelete.Enabled = True Else btnDelete.Enabled = False
+        End If
+    End Sub
+    Dim filestring As String
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles btnNew.Click
+        Select Case btnNew.Text
+            Case "NEW"
+                cmbTitles.Text = ""
+                txtTitleID.Text = ""
+                cmbTitles.Focus()
+                btnNew.Text = "CANCEL"
+                btnDelete.Text = "SAVE"
+                btnDelete.Enabled = False
+            Case "CANCEL"
+                LoadTitles()
+                btnNew.Text = "NEW"
+                btnDelete.Text = "DELETE"
+                cmbTitles.Focus()
+        End Select
+    End Sub
+
+    Private Sub Button1_Click_2(sender As Object, e As EventArgs) Handles btnDelete.Click
+        Select Case btnDelete.Text
+            Case "DELETE"
+                If MsgBox("Do you want to delete this game?" & vbCrLf & "Game Title: " & cmbTitles.Text & vbCrLf & "Game ID: " & txtTitleID.Text, vbQuestion + vbYesNo) = vbYes Then
+                    Dim lines As List(Of String) = System.IO.File.ReadAllLines(DBGameIDPath).ToList
+                    lines.RemoveAt(cmbTitles.SelectedIndex) ' index starts at 0 
+                    System.IO.File.WriteAllLines(DBGameIDPath, lines)
+                    MsgBox("Game Title: " & cmbTitles.Text & " with Game ID: " & txtTitleID.Text & vbCrLf & "has been deleted!", vbInformation, "Deleted")
+                    LoadTitles()
+                End If
+            Case "SAVE"
+                Try
+                    Dim xRead As IO.StreamReader
+                    Dim words As New Dictionary(Of String, String)()
+                    xRead = File.OpenText(DBGameIDPath)
+                    Do Until xRead.EndOfStream
+                        Dim strArr() As String = xRead.ReadLine.Split(""",""")
+                        If strArr(1) = txtTitleID.Text Then
+                            MsgBox("Game ID: " & txtTitleID.Text & " is already exist!")
+                            xRead.Close()
+                            Exit Sub
+                        End If
+                    Loop
+                    xRead.Close()
+
+                Catch ex As Exception
+
+                End Try
+
+
+                Dim append As Boolean = False
+                If (System.IO.File.Exists(DBGameIDPath)) Then
+                    append = True
+                End If
+                Using xWrite As System.IO.StreamWriter = New System.IO.StreamWriter(DBGameIDPath, append)
+                    xWrite.WriteLine("{""" & txtTitleID.Text & """, """ & cmbTitles.Text & """}")
+                End Using
+                MsgBox("Saved!", vbInformation)
+                LoadTitles()
+                btnNew.Text = "NEW"
+                btnDelete.Text = "DELETE"
+        End Select
+
+    End Sub
+
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        If FolderBrowserDialog1.ShowDialog() = DialogResult.OK Then
+            txtExefsPath.Text = FolderBrowserDialog1.SelectedPath
+        End If
+    End Sub
+
+    Private Sub Button1_Click_3(sender As Object, e As EventArgs) Handles btnContextMenu.Click
+        If (isElevated) Then
+            Select Case btnContextMenu.Text
+                Case "Add to Context Menu"
+                    My.Computer.Registry.ClassesRoot.CreateSubKey("Folder\shell\Patch Game ID\command")
+                    My.Computer.Registry.SetValue("HKEY_CLASSES_ROOT\Folder\shell\Patch Game ID", "Icon", Application.ExecutablePath)
+                    My.Computer.Registry.SetValue("HKEY_CLASSES_ROOT\Folder\shell\Patch Game ID\command", "", """" & Application.ExecutablePath & """ ""%1""")
+                    MsgBox("Successfully added!", vbInformation)
+                    btnContextMenu.Text = "Remove from Context Menu"
+                Case "Remove from Context Menu"
+                    My.Computer.Registry.ClassesRoot.DeleteSubKey("Folder\shell\Patch Game ID")
+                    MsgBox("Successfully removed!", vbInformation)
+                    btnContextMenu.Text = "Add to Context Menu"
+            End Select
+        Else
+            If MsgBox("Program requires Administrator privileges to continue, click OK to restart the program with administrator privileges", vbInformation + vbOKCancel, "Notice") = vbOK Then RestartElevated()
+        End If
+    End Sub
+    Public Sub RestartElevated()
+        Dim startInfo As ProcessStartInfo = New ProcessStartInfo()
+        startInfo.UseShellExecute = True
+        startInfo.WorkingDirectory = Environment.CurrentDirectory
+        startInfo.FileName = Application.ExecutablePath
+        startInfo.Verb = "runas"
+        Try
+            Dim p As Process = Process.Start(startInfo)
+        Catch ex As Exception
+            Return 'If cancelled, do nothing
+        End Try
+        Application.Exit()
+    End Sub
+
+End Class
